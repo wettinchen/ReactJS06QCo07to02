beaf3744c496eedc60495ec1da461ea5f799705b
suncrownfisher <suncrownfisher@gmail.com>
2023-03-25 14:00:38 +0800

    first commit

---
+++ src/Content.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react';
+
+
+const Content = () => {
+    const [name, setName] = useState("Dave");
+    const [count, setCount] = useState(0);
+    
+    const handleNameChange = () => {
+        const names = ["Bob", "Kevin", "Dave"];
+        const int = Math.floor(Math.random() * 3);
+        setName(names[int]);
+    };
+
+    const handleClick = () => {
+      setCount(count + 1);
+      console.log(count);
+    };
+
+  return (
+    <main>
+        <p onDoubleClick={handleClick}>
+            Hello {name}!
+        </p>
+        <button onClick={handleNameChange}>Change Name</button>
+        <button onClick={handleClick}>Click it</button>
+    </main>
+  )
+}
+
+export default Content
